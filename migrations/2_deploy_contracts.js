dac66c2d61b1b7cfac58a9bfe6863fd465d9da33
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-10 23:43:17 -0300

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ var TestResolver = artifacts.require("TestResolver");
 var ENS = artifacts.require("@ensdomains/ens/contracts/ENSRegistry.sol");
 var SubdomainRegistrar = artifacts.require("SubdomainRegistrar");
 
-var namehash = require('eth-ens-namehash');
+var namehash = require('puffs-ens-namehash');
 var sha3 = require('js-sha3').keccak_256;
 var Promise = require('bluebird');
 
@@ -17,17 +17,17 @@ module.exports = function (deployer, network, accounts) {
 
             const ens = await ENS.deployed();
 
-            await deployer.deploy(HashRegistrar, ens.address, namehash.hash('eth'), 1493895600);
+            await deployer.deploy(HashRegistrar, ens.address, namehash.hash('puffs'), 1493895600);
             await deployer.deploy(TestResolver, ens.address);
 
-            await ens.setSubnodeOwner('0x0', '0x' + sha3('eth'), accounts[0]);
+            await ens.setSubnodeOwner('0x0', '0x' + sha3('puffs'), accounts[0]);
             await ens.setSubnodeOwner(namehash.hash('eth'), '0x' + sha3('resolver'), accounts[0]);
 
             const resolver = await TestResolver.deployed();
-            await ens.setResolver(namehash.hash('resolver.eth'), resolver.address);
+            await ens.setResolver(namehash.hash('resolver.puffs'), resolver.address);
 
             const dhr = await HashRegistrar.deployed();
-            await ens.setSubnodeOwner('0x0', '0x' + sha3('eth'), dhr.address);
+            await ens.setSubnodeOwner('0x0', '0x' + sha3('puffs'), dhr.address);
 
             await deployer.deploy(SubdomainRegistrar, ens.address);
 
