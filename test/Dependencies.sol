1098f264c75bb6f0bb98ff206e22e54484191ee1
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-10 23:49:57 -0300

    Update Dependencies.sol

--- test/Dependencies.sol
+++ test/Dependencies.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.5.0;
 
-import "@ensdomains/ens/contracts/ENSRegistry.sol";
-import "@ensdomains/ens/contracts/HashRegistrar.sol";
-import "@ensdomains/ethregistrar/contracts/BaseRegistrarImplementation.sol";
+import "@puffscoin/ens/contracts/ENSRegistry.sol";
+import "@puffscoin/ens/contracts/HashRegistrar.sol";
+import "@puffscoin/puffsregistrar/contracts/BaseRegistrarImplementation.sol";
 
 contract Dependencies {
     constructor() public { }
